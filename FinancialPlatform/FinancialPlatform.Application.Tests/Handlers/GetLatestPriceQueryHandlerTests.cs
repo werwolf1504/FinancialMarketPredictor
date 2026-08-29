a8950bd815827741b5a0892d2940bc251b9f4e32
Ihor Pereverten <ihorpereverten@gmail.com>
2026-08-29 19:48:32 +0200

    Add unit tests for MediatR handlers. Resolves #14

---
+++ FinancialPlatform/FinancialPlatform.Application.Tests/Handlers/GetLatestPriceQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FinancialPlatform.Application.Interfaces;
+using FinancialPlatform.Application.Queries;
+using FinancialPlatform.Domain.Entities;
+
+using FluentAssertions;
+
+using Moq;
+
+namespace FinancialPlatform.Application.Tests.Handlers;
+
+public class GetLatestPriceQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldReturnMarketTick_WhenTickerIsValid()
+    {
+        // Arrange
+        var ticker = "AAPL";
+        var expectedMarketTick = new MarketTick
+        {
+            Ticker = ticker,
+            Price = 150.00m
+        };
+
+        var mockMarketDataProvider = new Mock<IMarketDataProvider>();
+        mockMarketDataProvider.Setup(m => m.GetCurrentPriceAsync(ticker))
+            .ReturnsAsync(expectedMarketTick);
+
+        var handler = new GetLatestPriceQueryHandler(mockMarketDataProvider.Object);
+        var query = new GetLatestPriceQuery(ticker);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Ticker.Should().Be(ticker);
+        result.Price.Should().Be(expectedMarketTick.Price);
+    }
+}
---
+++ FinancialPlatform/FinancialPlatform.Application.Tests/Handlers/SaveMarketDataCommandHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FinancialPlatform.Application.Commands;
+using FinancialPlatform.Application.Interfaces;
+using FinancialPlatform.Contracts;
+using FinancialPlatform.Domain.Entities;
+
+using MassTransit;
+
+using Moq;
+
+namespace FinancialPlatform.Application.Tests.Handlers;
+
+public class SaveMarketDataCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldSaveMarketTick_WhenTickerIsValid()
+    {
+        // Arrange
+        var mockRepository = new Mock<IMarketDataRepository>();
+        var mockPublishEndpoint = new Mock<IPublishEndpoint>();
+
+        var tickToSave = new MarketTick
+        {
+            Ticker = "NVDA",
+            Price = 450.00m,
+            Timestamp = DateTime.UtcNow
+        };
+
+        var command = new SaveMarketDataCommand(tickToSave);
+
+        var handler = new SaveMarketDataCommandHandler(mockRepository.Object, mockPublishEndpoint.Object);
+
+        await handler.Handle(command, CancellationToken.None);
+
+        mockRepository.Verify(repo => repo.InsertASync(It.Is<MarketTick>(mt => mt.Ticker == "NVDA")), Times.Once);
+
+        mockPublishEndpoint.Verify(pub => pub.Publish(It.Is<MarketDataCollectedEvent>(mt => mt.Ticker == "NVDA" && mt.Price == 450.00m), It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
